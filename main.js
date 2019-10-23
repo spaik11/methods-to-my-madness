c86ae44002ddaa7290f180a323a79f028f97879b
Sung Paik <sung.paik@codeimmersives.com>
2019-10-23 15:59:46 -0400

    struggled with this exercise

--- main.js
+++ main.js
@@ -1,23 +1,76 @@
 function slice(string, start = 0, end = string.length) {
+  let newStr = '';
+
+  for (let i = start; i < end; i++) {
+    newStr += string[i];
+  }
+  return newStr;
 }
 
 function repeat(str, repetitions) {
+  let newStr = '';
+
+  for (let i = 0; i < repetitions; i++) {
+    newStr += str
+  }
+  return newStr;
 }
 
 function startsWith(string, substring) {
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === substring[i]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function endsWith(str, substring) {
+  for (let i = str.length - 1; i > 0; i--) {
+    if (str[i] === substring[i]) {
+      return true;
+    }
+  }
+  return false;
 }
 
+
 function includes(arr, item) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function join(arr, separator = '') {
+  let newStr = '';
+
+  if (separator === '') {
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] === arr.length - 1) {
+      newStr += arr[i];
+    } else {
+      newStr += `${arr[i]}${separator}`;
+    } 
+  }
+}
+  return newStr;
 }
 
 
 function split(str, separator) {
+  const newArr = [];
+
+  for (let i = 0; i < 1; i++) {
+    if (separator === undefined) {
+      newArr.push(str)
+    } else {
+      newArr.push(str[separator])
+    }
+  }
+  return newArr;
 }
 
 function trimStart(str) {
